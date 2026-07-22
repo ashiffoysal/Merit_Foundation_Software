2bac0218637a5ac8d5cee0992bcadcffcddfa39f
ashiffoysal <36834363+ashiffoysal@users.noreply.github.com>
2026-07-22 14:15:41 +0600

    User Admin panel Upgrade

--- resources/views/frontend/user_dashboard/index.blade.php
+++ resources/views/frontend/user_dashboard/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.frontend')
-@section('title', 'Privacy Policy - Merit Education Foundation')
+@section('title', 'Dashboard - Merit Education Foundation')
 @section('content')
 <!-- jQuery (load BEFORE your script) -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
